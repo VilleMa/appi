ddf47b46377bf20934d2d9d8ac8a97a6f026de20
VilleMa <36548229+VilleMa@users.noreply.github.com>
2021-02-13 11:48:00 +0200

    vika

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,44 +1,41 @@
 import React, { useState, useEffect } from "react";
 import Askcity from "./Askcity";
+const axios = require("axios");
 
 function Content() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
   const [weatherdata, setWeatherdata] = useState([]);
+
+  async function fetchMovies() {
+    const url = `/.netlify/functions/getWeather`;
+    const response = await axios.get(url);
+    const data = response.data;
+    setWeatherdata(data);
+  }
+
   // Note: the empty deps array [] means this useEffect will run once(similar to componentDidMount())
   useEffect(() => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${process.env.REACT_APP_CITY}&appid=${process.env.REACT_APP_API_KEY}`
-    )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setWeatherdata(result.main);
-          //console.log(weatherdata);
-        },
-        // Note: it's important to handle errors here instead of a catch() block so that we don't swallow exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
+    fetchMovies();
   }, []);
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div className="Content-style">
-        <Askcity />
-        <ul className="liststyle">
-          <li>Temperature {weatherdata.temp - 273.15}</li>
-          <li>Humidity {weatherdata.humidity} %</li>
-          <li></li>
-        </ul>
-      </div>
-    );
-  }
+
+  return (
+    <>
+      {weatherdata === null ? (
+        <div className="loading">
+          <h2>Loading ...</h2>
+        </div>
+      ) : (
+        <>
+          <div className="Content-style">
+            <Askcity />
+            <ul className="liststyle">
+              <li>Temperature {weatherdata.temp - 273.15}</li>
+              <li>Humidity {weatherdata.humidity} %</li>
+              <li></li>
+            </ul>
+          </div>
+        </>
+      )}
+    </>
+  );
 }
 export default Content;
